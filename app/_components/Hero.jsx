c93e3bf34eb6a8bdb1e2fd05f59433c6d6894542
ravinder <ravibeniwal931@gmail.com>
2025-02-16 08:35:00 +0530

    auth update

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import { TextGenerateEffect } from '@/components/ui/TextGenerateEffect'
-import { BackgroundBeamsWithCollision } from '@/components/ui/BackgroundBeams'
+import { BackgroundBeamsWithCollision } from '@/components/ui/BackgroundBeams'  
 
 const Hero = () => {
   return (
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import { ClerkProvider, GoogleOneTap } from "@clerk/nextjs";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -20,7 +21,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <ClerkProvider>
+      <html lang="en">
+        <GoogleOneTap/>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
@@ -34,5 +37,7 @@ export default function RootLayout({ children }) {
         </ThemeProvider>
       </body>
     </html>
+    </ClerkProvider>
+    
   );
 }
