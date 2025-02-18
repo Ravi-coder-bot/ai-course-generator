95608fbfd940803c2937cdaca72176e3d2847b80
ravinder <ravibeniwal931@gmail.com>
2025-02-18 18:05:39 +0530

    create-course-page

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
         <div className="mt-8 flex flex-wrap justify-center gap-4">
           <a
             className="block w-full rounded-sm bg-primary px-12 py-3 text-sm font-medium text-white shadow-sm hover:bg-red-700 focus:ring-3 focus:outline-hidden sm:w-auto"
-            href="#"
+            href="/dashboard"
           >
             Get Started
           </a>
--- app/create-course/page.jsx
+++ app/create-course/page.jsx
@@ -1,13 +1,68 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
+import { HiViewGrid } from "react-icons/hi";
+import { HiLightBulb } from "react-icons/hi";
+import { HiClipboardCheck } from "react-icons/hi";
+import { Button } from '@/components/ui/button';
 
 const CreateCourse = () => {
+  const StepperOptions =[
+    {
+      id : 1,
+      title : "Category",
+      description : "Select a category for your course",
+      icon : <HiViewGrid />
+    },
+    {
+      id : 2,
+      title : "Topic and Desc",
+      description : "Write a description and topic for your course",
+      icon : <HiLightBulb />
+    },
+    {
+      id : 3,
+      title : "Options",
+      description : "Select options for your course",
+      icon : <HiClipboardCheck />
+    }
+  ]
+
+  const [activeIndex,setActiveIndex]  = useState(0)
   return (
     <div>
-      <div>
-        <div>
-          <h2>Create Course</h2>
+
+        <div className='flex flex-col justify-center items-center mt-10'>
+          <h2 className='text-primary text-4xl font-medium'>Create Course</h2>
+          <div className='flex mt-10'>
+            {StepperOptions.map((item,index)=>(
+              <div className='flex items-center'>
+                <div className='flex flex-col items-center w-[50px] md:w-[100px]'>
+                  <div className={`bg-gray-200 rounded-full p-3 text-white ${activeIndex>=index && "bg-purple-500"}`}>
+                     {item.icon}
+                  </div>
+                  <h2 className='hidden md:block md:text-sm'>{item.title}</h2>
+                </div>
+                {index!= StepperOptions.length-1 && 
+                <div className={`h-1 w-[50px] md:w-[100px] lg:w-[170px] bg-gray-300 rounded-full ${activeIndex>=index+1 && "bg-purple-500"}`}>
+                </div>
+                }
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
+        <div className='px-10 md:px-20 lg:px-44 mt-10'>
+
+       
+
+
+        <div className='flex justify-between mt-10'>
+          <Button disabled={activeIndex == 0} onClick={() => setActiveIndex(activeIndex - 1)} className="hover:bg-primary hover:text-primary-foreground"
+          variant="outline"
+            >Previous</Button>
+          {activeIndex<2 && <Button onClick={() => setActiveIndex(activeIndex + 1)}>Next</Button>}
+          {activeIndex==2 && <Button>Gnerate Course Layout</Button>}
+        </div>
+       </div>
     </div>
   )
 }
