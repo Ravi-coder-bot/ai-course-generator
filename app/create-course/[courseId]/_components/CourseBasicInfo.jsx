ddcb8711061ec8b138b3000cc2f1caf7e04a7a63
ravinder <ravibeniwal931@gmail.com>
2025-03-30 18:26:00 +0530

    making seprate page for each course

---
+++ app/create-course/[courseId]/_components/CourseBasicInfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CourseBasicInfo = ({course}) => {
+  return (
+    <div>CourseBasicInfo</div>
+  )
+}
+
+export default CourseBasicInfo
--- app/create-course/[courseId]/page.jsx
+++ app/create-course/[courseId]/page.jsx
@@ -1,8 +1,50 @@
+"use client"
 import React from 'react'
+import { useEffect ,useState } from 'react'
+import { useUser } from '@clerk/nextjs'
+import { and, eq } from 'drizzle-orm'
+import { CourseList } from '@/configs/schema'
+import { db } from '@/configs/db'
+import LoadingDialog from '../_components/LoadingDialog'
+import { Button } from '@/components/ui/button'
+import CourseBasicInfo from './_components/CourseBasicInfo'
 
-const CourseLayout = () => {
+
+const CourseLayout = ({params}) => {
+    const { user } = useUser();
+    const [course, setCourse] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+          params && GetCourse();
+        },[params,user]);
+
+    const GetCourse = async () => {
+        const result = await db.select().from(CourseList)
+        .where(and(eq(CourseList.courseId, params?.courseId),
+                eq(CourseList?.createdBy, user?.primaryEmailAddress?.emailAddress)));
+                setCourse(result[0]);
+                
+                console.log(result);
+
+    }
   return (
-    <div>CourseLayout</div>
+     <>
+          <LoadingDialog loading={loading} />
+          <div className="mt-10 px-7 md:px-20 lg:px-44">
+            <h2 className="font-bold text-center text-2xl">Course Layout</h2>
+            {/* Basic Info */}
+            <CourseBasicInfo course={course} />
+            
+            {/* Course Detail */}
+            
+            {/* List of Lesson */}
+           
+            <Button  className="my-10">
+              Generate Course Content
+            </Button>
+          </div>
+        </>
   )
 }
 
