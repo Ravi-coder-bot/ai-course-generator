a34a21e2c5c466caf5b448ed0c1af3af85a305f4
ravinder <ravibeniwal931@gmail.com>
2025-03-31 13:46:00 +0530

    uploading images

--- app/create-course/[courseId]/_components/CourseBasicInfo.jsx
+++ app/create-course/[courseId]/_components/CourseBasicInfo.jsx
@@ -3,11 +3,21 @@ import Image from 'next/image'
 import { HiOutlinePuzzle } from "react-icons/hi";
 import { Button } from "@/components/ui/button";
 import EditCourseBasicInfo from './EditCourseBasicInfo';
+import { useState } from 'react';
 
 
 
 
 const CourseBasicInfo = ({course, refreshData}) => {
+
+    const [selectedFile, setSelectedFile] = useState(null);
+    
+    const onFileSelected = (e) => {
+        const file = e.target.files[0];
+        setSelectedFile(URL.createObjectURL(file));
+        refreshData(true);
+      };
+
   return (
     <div className='p-10 border rounded-xl shadow-sm mt-5'>
         <div className='grid grid-col-1 md:grid-cols-2 gap-10'>
@@ -27,8 +37,12 @@ const CourseBasicInfo = ({course, refreshData}) => {
                           <Button className="w-full mt-5">Start</Button>
             </div>
             <div>
-                <Image src={'/placeholder.png'} width={300} height={300} alt="Course Banner"
-                className='w-full rounded-xl object-cover h-[250px] ' />
+                <label htmlFor="course-banner">
+                     <Image src={ selectedFile ? selectedFile : '/placeholder.png'} width={300} height={300} alt="Course Banner"
+                className='w-full rounded-xl object-cover h-[250px] cursor-pointer' />
+                </label>
+               
+                <input type="file" className="opacity-0" id="course-banner" onChange={onFileSelected}/>
 
 
             </div>
