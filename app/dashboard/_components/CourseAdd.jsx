2eff8f6f5f9276db0aa04876131f1c100dac407b
ravinder <ravibeniwal931@gmail.com>
2025-03-31 20:06:47 +0530

    blocking added

--- app/dashboard/_components/CourseAdd.jsx
+++ app/dashboard/_components/CourseAdd.jsx
@@ -3,8 +3,11 @@ import { useUser } from '@clerk/nextjs'
 import React from 'react'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
+import { useContext } from 'react'
+import { UserCourseListContext } from '@/app/_context/UserCourseListContext'
 
 const CourseAdd = () => {
+    const { userCourseList, setUserCourseList } = useContext(UserCourseListContext);
     const {user} = useUser();
   return (
     <div className='flex item-center justify-between'>
@@ -12,7 +15,9 @@ const CourseAdd = () => {
             <h1 className='text-3xl'>Hello, <span className='font-bold'>{user?.fullName}</span></h1>
             <p className='text-sm text-gray-500'>Create new course with AI , share with friends and earn from it </p>
         </div>
-        <Link href={'./create-course'}>
+        <Link href={
+              userCourseList?.length >= 5 ? "/dashboard/upgrade" : "/create-course"
+            }>
          <Button>+ Create AI Course</Button>
         </Link>
        
